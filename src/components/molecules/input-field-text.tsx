49a29b1b5321c92c78c03ab59ee31b1ddcce99e6
kjoobin <imkj1148@gmail.com>
2021-03-27 19:52:10 +0900

    fix typo

--- src/components/molecules/input-field-text.tsx
+++ src/components/molecules/input-field-text.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { TextField, TextFieldProps } from "@material-ui/core";
 
-export type InputFieldText = TextFieldProps1;
+export type InputFieldText = TextFieldProps;
 
 export const InputFieldText:React.FunctionComponent<TextFieldProps> = React.memo((props) => {
   return <TextField {...props} />;
