042ca32c7da0f071ebdf8e956af238b5234982c3
kjoobin <imkj1148@gmail.com>
2021-03-28 00:05:07 +0900

    add axios for search data

--- src/pages/search/skill/index.tsx
+++ src/pages/search/skill/index.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Box, Container, Text } from "@component/atoms";
 import { InputFieldText } from "@component/molecules";
+import axios from "axios";
 import { useRouter } from "next/router";
 
 export default function SearchWarlords() {
   const route = useRouter();
+
   console.log({ route });
+
+  const [temp, setTemp] = useState<string>("");
+
+  const onChangeText = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setTemp(e.target.value);
+  };
+
+  useEffect(() => {
+    if (temp) {
+      axios.get(`/api/skill?q=${temp}`).then((res) => {
+        console.log(res);
+      });
+    }
+  }, [temp]);
+
+  useEffect(() => {
+    const { q } = route.query;
+    if (q && typeof q === "string" ) {
+      setTemp(q);
+    }
+  }, [route.query.q]);
 
   return (
     <Container>
@@ -17,7 +40,14 @@ export default function SearchWarlords() {
 
       </Box>
       <Box p={1}>
-        <InputFieldText label={"장수 이름 또는 전법 이름"} required fullWidth/>
+        <InputFieldText
+          required
+          fullWidth
+          label={"장수 이름 또는 전법 이름"}
+          InputLabelProps={{ shrink: true }}
+          value={temp}
+          onChange={onChangeText}
+        />
       </Box>
     </Container>
   );
