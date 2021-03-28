fd810508d020d9f2069eec412bc972238af87bc4
kjoobin <imkj1148@gmail.com>
2021-03-28 15:11:32 +0900

    move header in global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from "next/app";
+import { Head } from "next/document";
 
 import { FirebaseProvider } from "../provider/firebase";
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return <FirebaseProvider>
+    <Head>
+      <title>모바일 삼국지 전략판 유틸 기능</title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <script data-ad-client="ca-pub-3235703528769525" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    </Head>
     <Component {...pageProps} />
   </FirebaseProvider>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>모바일 삼국지 전략판 유틸 기능</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </Head>
-
       <main>
         <Container>
           <Box>
