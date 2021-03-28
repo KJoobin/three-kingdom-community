7b5953d566bff06aceed38b9c6171043c9aa7dc3
kjoobin <imkj1148@gmail.com>
2021-03-28 15:38:19 +0900

    add last updated at

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,32 @@
-import { Box, Container, Image } from "@component/atoms";
+import { Box, Container, Image, Text } from "@component/atoms";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <Container>
+    <Container>
+      <Box height={"calc(100vh - 16px)"} display={"flex"} flexDirection={"column"} justifyContent={"space-between"}>
+        <main>
           <Box>
-            <Image src={"./images/main-image.png"} width={"auto"} height={"auto"}/>
+            <Box maxWidth={"100vw"}>
+              <Image src={"./images/main-image.png"} width={"100%"} height={"100%"}/>
+            </Box>
+            <Box py={1}>
+              <Text variant={"h5"}>
+                <Link href={"search/skill"}>
+                전법 검색
+                </Link>
+              </Text>
+            </Box>
           </Box>
-          <Box py={1}>
-            <Link href={"search/skill"}>
-              전법 검색
-            </Link>
+        </main>
+        <footer>
+          <Box p={1}>
+            <Text>
+          마지막 업데이트 : 2021년 3월 28일
+            </Text>
           </Box>
-        </Container>
-      </main>
-    </div>
+        </footer>
+      </Box>
+    </Container>
   );
 }
