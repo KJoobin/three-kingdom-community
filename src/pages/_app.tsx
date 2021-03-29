10fe5ec085a1bc00f774402e12e7bb1c5a03eec2
KJoobin <45376611+KJoobin@users.noreply.github.com>
2021-03-29 16:32:20 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>모바일 삼국지 전략판 유틸 기능</title>
         <script data-ad-client="ca-pub-3235703528769525" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <meta name="google-site-verification" content="OZ4Ys5kfawMZ_LGL2QdQeJyG-GhaJTiX84ruYLgtHC8">
       </Head>
       <Component {...pageProps} />
     </>
