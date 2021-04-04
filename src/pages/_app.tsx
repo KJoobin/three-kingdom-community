c7c9d2365f813a26f95281f88b31ad29e8259cec
kjoobin <imkj1148@gmail.com>
2021-04-04 22:25:03 +0900

    remove header in __app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
 import { FirebaseProvider } from "src/provider/firebase";
 
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
-    <>
-      <Head>
-        <title>모바일 삼국지 전략판 유틸 기능</title>
-      </Head>
-      <FirebaseProvider>
-        <Component {...pageProps} />
-      </FirebaseProvider>
-    </>
+    <FirebaseProvider>
+      <Component {...pageProps} />
+    </FirebaseProvider>
   );
 }
